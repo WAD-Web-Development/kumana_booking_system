9b45d358d259de023d6a43151076c94b21cc6677
Shreevaka <k.m.g.shreevaka@gmail.com>
2025-06-27 16:32:34 +0530

    welcome page ui completed

--- resources/views/pages/welcome/index.blade.php
+++ resources/views/pages/welcome/index.blade.php
@@ -8,14 +8,15 @@
                 <div id="welcomeCarousel" class="carousel slide w-100 h-100" data-bs-ride="carousel">
                     <div class="carousel-inner h-100">
                         <div class="carousel-item active h-100 position-relative">
-                            <img src="{{ asset('assets/img/logo.jpg') }}" class="d-block w-100 h-100 object-fit-cover" alt="Slide 1">
+                            <img src="{{ asset('assets/img/image1.jpg') }}" class="d-block w-100 h-100 object-fit-cover" alt="Slide 1">
 
                             <div class="position-absolute top-0 start-0 w-100 h-100 d-flex flex-column justify-content-start align-items-start p-4 welcome-overlay">
-                                <div class="mt-auto mx-3">
-                                    <span class="d-block text-white fw-bold fs-5 welcome-slide-label">
-                                        1  4
-                                    </span>
-                                </div>
+                                <div class="mt-auto mx-3 d-flex align-items-center welcome-slide-label-container">
+                                    <span class="welcome-slide-label">01</span>
+                                    <div class="line-divider"></div>
+                                    <span class="welcome-slide-label">04</span>
+                                  </div>
+
                                 <div class="mx-3">
                                     <h1 class="welcome-slide-title">
                                         Book Your<br>Kumana Park Experience
@@ -29,19 +30,41 @@
                             <div class="position-absolute top-0 start-0 w-100 h-100 welcome-overlay-bg"></div>
                         </div>
 
-                        <div class="carousel-item h-100">
+                        <div class="carousel-item h-100 position-relative">
                             <img src="{{ asset('assets/img/image2.jpg') }}" class="d-block w-100 h-100 object-fit-cover" alt="Slide 2">
+
+                            <div class="position-absolute top-0 start-0 w-100 h-100 d-flex flex-column justify-content-start align-items-start p-4 welcome-overlay">
+                                <div class="mt-auto mx-3 d-flex align-items-center welcome-slide-label-container">
+                                    <span class="welcome-slide-label">02</span>
+                                    <div class="line-divider"></div>
+                                    <span class="welcome-slide-label">04</span>
+                                  </div>
+
+                                <div class="mx-3">
+                                    <h1 class="welcome-slide-title">
+                                        Book Your<br>Kumana Park Experience
+                                    </h1>
+                                    <p class="welcome-slide-desc">
+                                        Kumana National Park in Sri Lanka is renowned for its avifauna, particularly its large flocks of migratory waterfowl and wading birds. The park is 391 kilometres southeast of Colombo on Sri Lanka's southeastern coast. Kumana is contiguous with Yala National Park
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="position-absolute top-0 start-0 w-100 h-100 welcome-overlay-bg"></div>
                         </div>
-                        <!-- Add more images as needed -->
+
+                        {{-- <div class="carousel-item h-100">
+                            <img src="{{ asset('assets/img/image2.jpg') }}" class="d-block w-100 h-100 object-fit-cover" alt="Slide 2">
+                        </div> --}}
                     </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#welcomeCarousel" data-bs-slide="prev">
+                    {{-- <button class="carousel-control-prev" type="button" data-bs-target="#welcomeCarousel" data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Previous</span>
                     </button>
                     <button class="carousel-control-next" type="button" data-bs-target="#welcomeCarousel" data-bs-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Next</span>
-                    </button>
+                    </button> --}}
                 </div>
             </div>
 
