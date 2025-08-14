fd6e4976f8d1d5de31587e48372c64e812dd5578
Shreevaka <k.m.g.shreevaka@gmail.com>
2025-08-14 16:41:51 +0530

    email attachment completed

--- resources/views/pages/admin/email_attachments/create.blade.php
+++ resources/views/pages/admin/email_attachments/create.blade.php
@@ -1,47 +1,88 @@
-@extends('layouts.app', ['activePage' => 'dashboard', 'activeSection' => 'dashboard'])
+@extends('layouts.app-dashboard', ['activePage' => 'email_attachment', 'activeSection' => 'email_attachment'])
 
 @section('content')
-
-<div class="container-fluid admin-container d-flex justify-content-center">
-    <!-- Adjust the width of the card -->
-    <div class="card admin-card shadow-lg col-md-6 mb-5">
-        <div class="card-header bg-white border-0 p-4 pb-1">
-            <div class="d-lg-flex">
-                <div>
-                    <h5 class="mb-0 admin-title">New Email Attachment</h5>
+    <div class="container-fluid m-0 p-0">
+        <div class="row m-0 p-0">
+            <div class="col-12">
+                <div class="card admin-management-page-card">
+                    <div class="card-header px-4 py-3 d-flex justify-content-between align-items-center admin-management-page-card-header">
+                        <h5 class="admin-management-page-card-title mb-0">Create Email Attachment</h5>
+                    </div>
+                    <div class="card-body admin-management-page-card-body px-4 py-4">
+                        <form action="{{ route('email-attachment.store') }}" method="POST" id="email-attachment-form" enctype="multipart/form-data">
+                        @csrf
+                            <div class="row gx-3">
+                                <div class="col-12">
+                                    <div class="row input-group admin-management-page-card-input-row">
+                                        <div class="col-8 input-group-prepend admin-management-page-card-input-label">
+                                            <span class="input-group-text admin-management-page-card-input-label-text">Title</span>
+                                        </div>
+                                        <input type="text" class="col-4 form-control admin-management-page-card-input-value" aria-label="title" id="title" name="title" placeholder="Enter title">
+                                    </div>
+                                    @error('title')
+                                        <div class="invalid-feedback d-flex align-items-center mt-1 px-3 py-2" role="alert">
+                                            <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M10.042 18.6715C5.43958 18.6715 1.70862 14.9405 1.70862 10.3382C1.70862 5.73584 5.43958 2.00488 10.042 2.00488C14.6443 2.00488 18.3753 5.73584 18.3753 10.3382C18.3753 14.9405 14.6443 18.6715 10.042 18.6715ZM9.20862 12.8382V14.5049H10.8753V12.8382H9.20862ZM9.20862 6.17155V11.1715H10.8753V6.17155H9.20862Z" fill="white"/>
+                                            </svg>
+                                            <span class="invalid-feedback-text mx-2">{{ $message }}</span>
+                                        </div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-12">
+                                    <textarea id="description" name="description" class="form-control admin-management-page-card-description px-4 py-3" rows="8" placeholder="Enter description"></textarea>
+                                    @error('description')
+                                        <div class="invalid-feedback d-flex align-items-center mt-1 px-3 py-2" role="alert">
+                                            <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M10.042 18.6715C5.43958 18.6715 1.70862 14.9405 1.70862 10.3382C1.70862 5.73584 5.43958 2.00488 10.042 2.00488C14.6443 2.00488 18.3753 5.73584 18.3753 10.3382C18.3753 14.9405 14.6443 18.6715 10.042 18.6715ZM9.20862 12.8382V14.5049H10.8753V12.8382H9.20862ZM9.20862 6.17155V11.1715H10.8753V6.17155H9.20862Z" fill="white"/>
+                                            </svg>
+                                            <span class="invalid-feedback-text mx-2">{{ $message }}</span>
+                                        </div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="row gx-3 mt-3">
+                                <div class="col-12">
+                                    <div class="row input-group admin-management-page-card-input-row align-items-center">
+                                        <div class="col-8 input-group-prepend admin-management-page-card-input-label">
+                                            <span class="input-group-text admin-management-page-card-input-label-text">
+                                                Attachment File
+                                            </span>
+                                        </div>
+                                        <div class="col-4">
+                                            <input type="file"
+                                                class="form-control admin-management-page-card-input-value w-100"
+                                                aria-label="file"
+                                                id="file"
+                                                name="file"
+                                                placeholder="Enter file">
+                                        </div>
+                                    </div>
+                                    @error('file')
+                                        <div class="invalid-feedback d-flex align-items-center mt-1 px-3 py-2" role="alert">
+                                            <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M10.042 18.6715C5.43958 18.6715 1.70862 14.9405 1.70862 10.3382C1.70862 5.73584 5.43958 2.00488 10.042 2.00488C14.6443 2.00488 18.3753 5.73584 18.3753 10.3382C18.3753 14.9405 14.6443 18.6715 10.042 18.6715ZM9.20862 12.8382V14.5049H10.8753V12.8382H9.20862ZM9.20862 6.17155V11.1715H10.8753V6.17155H9.20862Z" fill="white"/>
+                                            </svg>
+                                            <span class="invalid-feedback-text mx-2">{{ $message }}</span>
+                                        </div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="row mt-4">
+                                <div class="col-12 d-flex justify-content-end">
+                                    <button type="submit" class="admin-management-page-card-submit-btn px-3">Create Email Attachment</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="card-body mt-2 p-4 pt-1">
-            <form action="{{ route('email-attachment.store') }}" method="POST" id="email-attachment-form" enctype="multipart/form-data">
-                @csrf
-                <div class="form-group col-md-12 mt-2">
-                    <label for="title" class="form-label">Title <span style="color: red">*</span></label>
-                    <input type="text" class="form-control" id="title" name="title" required>
-                </div>
-
-                <div class="form-group col-md-12 mt-2">
-                    <label for="description" class="form-label mt-2">Description <span
-                        style="color: rgb(163, 163, 163)">(optional)</span></label>
-                    <textarea id="description" name="description" class="form-control description" rows="5"></textarea>
-                </div>
-
-                <div class="form-group col-md-12 mt-2">
-                    <label for="file" class="form-label mt-2">Attachment File <span style="color: red">*</span></label>
-                    <input type="file" class="form-control" id="file" name="file" required>
-                </div>
-
-                <div class="d-flex justify-content-end mt-4">
-                    <a href="{{ url()->previous() }}" class="btn btn-light admin-btn-cancel text-white m-0">Cancel</a>
-                    <button type="submit" class="btn admin-btn m-0 ms-2">Create Email Attachment</button>
-                </div>
-            </form>
-        </div>
     </div>
-</div>
-
 @endsection
 
 @push('custom_scripts')
-{!! JsValidator::formRequest('App\Http\Requests\StoreEmailAttachmentRequest', '#email-attachment-form') !!}
+    <script>
+    </script>
 @endpush
--- resources/views/pages/admin/email_attachments/index.blade.php
+++ resources/views/pages/admin/email_attachments/index.blade.php
@@ -1,51 +1,52 @@
-@extends('layouts.app', ['activePage' => 'dashboard', 'activeSection' => 'dashboard'])
+@extends('layouts.app-dashboard', ['activePage' => 'email_attachment', 'activeSection' => 'email_attachment'])
 
 @section('content')
-    <div class="container-fluid admin-container d-flex justify-content-center">
-        <div class="card admin-card w-100 shadow-lg">
-            <div class="card-header bg-white border-0 p-4">
-                <div class="d-flex justify-content-between align-items-center">
-                    <h5 class="mb-0 admin-title">Email Attachments Management</h5>
-                    @if (!$attachment)
-                        <a href="{{ route('email-attachment.create') }}" class="btn admin-btn btn-sm mb-0 px-4">+&nbsp; Add New</a>
-                    @endif
-                </div>
-            </div>
+    <div class="container-fluid m-0 p-0">
+        <div class="row m-0 p-0">
+            <div class="col-12">
+                <div class="card admin-management-page-card">
+                    <div class="card-header px-4 py-3 d-flex justify-content-between align-items-center admin-management-page-card-header">
+                        <h5 class="admin-management-page-card-title mb-0">Email Attachments</h5>
+                        @if (!$attachment)
+                            <a href="{{ route('email-attachment.create') }}" class="admin-management-page-card-new-btn">Add New</a>
+                        @endif
+                    </div>
+                    <div class="card-body admin-management-page-card-body px-4 py-4">
 
-            <div class="card-body p-4">
+                        <div class="table-responsive table-wrapper">
+                            <table class="table table-bordered table-hover table-responsive admin-management-table" id="email-attachment-list">
+                                <thead>
+                                    <tr>
+                                        <th>Title</th>
+                                        <th width="10%">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @if ($attachment)
+                                        <tr id="row{{ $attachment->id }}">
+                                            <td> {{ $attachment->title }} </td>
+                                            <td class="text-sm">
 
-                <div class="table-responsive table-wrapper">
-                    <table class="table table-bordered table-hover table-responsive" id="email-attachment-list">
-                        <thead>
-                            <tr>
-                                <th>Title</th>
-                                <th width="6%">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @if ($attachment)
-                                <tr id="row{{ $attachment->id }}">
-                                    <td> {{ $attachment->title }} </td>
-                                    <td class="text-sm">
+                                                <a href="{{ route('email-attachment.edit', $attachment->id) }}" class="me-3"
+                                                    data-bs-toggle="tooltip" data-bs-original-title="Edit">
+                                                    <i class="fas fa-user-edit text-secondary"></i>
+                                                </a>
 
-                                        <a href="{{ route('email-attachment.edit', $attachment->id) }}" class="me-3"
-                                            data-bs-toggle="tooltip" data-bs-original-title="Edit">
-                                            <i class="fas fa-user-edit text-secondary"></i>
-                                        </a>
+                                                <a class="delete" data-id="{{ $attachment->id }}" href="#">
+                                                    <i class="fas fa-trash text-secondary"></i>
+                                                </a>
 
-                                        <a class="delete" data-id="{{ $attachment->id }}" href="#">
-                                            <i class="fas fa-trash text-secondary"></i>
-                                        </a>
-
-                                    </td>
-                                </tr>
-                            @else
-                                <tr>
-                                    <td colspan="5" class="text-center">No Data Available</td>
-                                </tr>
-                            @endif
-                        </tbody>
-                    </table>
+                                            </td>
+                                        </tr>
+                                    @else
+                                        <tr>
+                                            <td colspan="5" class="text-center">No Data Available</td>
+                                        </tr>
+                                    @endif
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -68,7 +69,8 @@
                 text: "You won't be able to revert this!",
                 customClass: {
                     popup: 'custom-swal-popup',
-                }
+                },
+                confirmButtonColor: '#0D4B2D',
             }).then((result) => {
                 if (result.isConfirmed) {
                     $.ajax({
@@ -109,28 +111,5 @@
                 }
             });
         });
-
-        // $('body').on('focusout', '#table_search', function() {
-        //     if ($(this).val() != $(this).attr('data-pre-search')) {
-        //         serachTable();
-        //     }
-        // });
-
-        // $('body').on('keypress', '#table_search', function(e) {
-        //     if (e.which == 13) {
-        //         if ($(this).val() != $(this).attr('data-pre-search')) {
-        //             serachTable();
-        //         }
-        //     }
-        // });
-
-        // function serachTable() {
-        //     var sr = $('#table_search').val();
-        //     var params = {
-        //         'sr': sr
-        //     };
-        //     var new_url = '{{ route('email-attachment.index') }}?' + jQuery.param(params);
-        //     window.location.href = new_url;
-        // }
     </script>
 @endpush
